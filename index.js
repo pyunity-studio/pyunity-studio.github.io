0d7bf44fb407332fadd1b25a1ba7ef4be83cc7cb
Pyunity Studio <84147095+pyunitystudio@users.noreply.github.com>
2022-09-17 00:09:27 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-
-const install_n8n = execSync('npm install n8n -g', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', install_n8n);
-const run_n8n = execSync('n8n start --tunnel', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', run_n8n);
-
